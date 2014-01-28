e8ef4ce63c850bcea17951b57d57e20ceca5e81a
Spencer Wright <snwright@gmail.com>
2014-01-28 11:19:35 -0500

    added description
    added a big comment at the top of the code that describes basic
    functionality, etc

--- PR4001_Radio_Test/PR4001_Radio_Test.ino
+++ PR4001_Radio_Test/PR4001_Radio_Test.ino
@@ -1,3 +1,25 @@
+/*
+THE PUBLIC RADIO
+WORKING TEST CODE
+Written by Spencer Wright and Zach Dunham
+with thanks to SparkFun's si470x team
+
+This code runs on our semi-custom hardware (PR5001 and PR5003) but can also be used
+on Sparkfun+Adruino parts.
+
+The basic rundown is as follows:
+  -Intialize everything on the Arduino
+  -Intialize si470x
+  -Enable shutdown on the amp
+  -Power up si470x
+  -Set tuning frequency & set volume on si470x
+  -Disable shutdown on the amp
+  -Turn the MCU off for power savings
+  -The MCU never gets to the loop
+
+LAST EDITED 2014.01.28
+*/
+
 #include <PR_Header.h>                      //this is our custom library, adapted from sparkfun
 #include <avr/sleep.h>                      //the standard AVR sleep library
 #include <Wire.h>                           //for i2c communication with si470x
