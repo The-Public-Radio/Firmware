9adab281f0a7477350209759a77872652dcc8678
Spencer Wright <snwright@gmail.com>
2014-01-28 10:44:43 -0500

    completed sleep functionality!
    needed to call sleepNow in the setup :/
    
    also improved comments significantly

--- PR4001_Radio_Test/PR4001_Radio_Test.ino
+++ PR4001_Radio_Test/PR4001_Radio_Test.ino
@@ -1,40 +1,37 @@
-#include <PR_Header.h>
-#include <avr/sleep.h>
-#include <Wire.h>
+#include <PR_Header.h>                      //this is our custom library, adapted from sparkfun
+#include <avr/sleep.h>                      //the standard AVR sleep library
+#include <Wire.h>                           //for i2c communication with si470x
 
 
-int resetPin = 12;
-int SDIO = A4;
-int SCLK = A5;
-int shutdown = 11;
-int led = 13;
+int resetPin = 12;                          //this is the reset pin for si470x
+int SDIO = A4;                              //for i2c with si470x
+int SCLK = A5;                              //for i2c with si470x
+int shutdown = 11;                          //the shutdown pin for the amp, used to mute output duing powerup
+int led = 13;                               //not used - for debugging only
 
-Public_Radio radio(resetPin, SDIO, SCLK);
-int channel = 883;
-int volume = 15;
+Public_Radio radio(resetPin, SDIO, SCLK);   //initializing the radio
+int channel = 971;                          //setting channel. 97.1 MHz = "971"
+int volume = 15;                            //setting volume, 0-15. 15 works well
 
 void setup()
 {
-  pinMode(shutdown, OUTPUT);
-  digitalWrite(shutdown, LOW);
-  radio.powerOn();
-  radio.setVolume(volume);
-  radio.setChannel(channel);
-  digitalWrite(shutdown, HIGH);
+  pinMode(shutdown, OUTPUT);                //setting the amp shutdown pin
+  digitalWrite(shutdown, LOW);              //shutting down the amp
+  radio.powerOn();                          //powering on si470x
+  radio.setVolume(volume);                  //setting output volume on si470x
+  radio.setChannel(channel);                //setting channel on si470x
+  digitalWrite(shutdown, HIGH);             //disabling shutdown on amp (turns amp on) 
+  sleepNow();                               //sleeps the MCU
 }
 
-void sleepNow()         // here we put the arduino to sleep
+void sleepNow()                             //the sleep function
 {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();          // enables the sleep bit in the mcucr register
-                             // so sleep is possible. just a safety pin
-  sleep_mode();            // here the device is actually put to sleep!!
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);      //this is the most power-saving mode in avr/sleep.h
+  sleep_enable();                           //enables the sleep bit in the mcucr register
+                                            //so sleep is possible. just a safety pin
+  sleep_mode();                             //here the device is actually put to sleep!!
 }
 
-void loop()
-{
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
+void loop()                                  //this needs to be in the code but is unused
+{                                            //the processor has already been put to sleep
 }
